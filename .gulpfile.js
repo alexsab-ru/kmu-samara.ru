ada612a5b901d7ea8fb34f8de5eb7fde3d044ad9
Alexander Sabanov <alexsab@mail.ru>
2020-04-26 16:31:51 +0400

    add gulpfile

---
+++ .gulpfile.js
@@ -0,0 +1,14 @@
+		scripts: {
+			src: [
+				base.kmu + '/libs/jquery/dist/jquery.min.js',
+				base.kmu + '/libs/Magnific-Popup-master/jquery.magnific-popup.js',
+				'node_modules/slick-1.8.1/slick/slick.min.js',
+				base.kmu + '/libs/animate/animate-css.js',
+				base.kmu + '/libs/lazyload.min.js',
+				base.kmu + '/libs/waypoint.js',
+				base.kmu + '/js/map.js',
+				base.kmu + '/js/common.js', // Custom scripts. Always at the end
+			],
+			dest:       base.kmu + '/js',
+			output:     'scripts.min.js',
+		},
