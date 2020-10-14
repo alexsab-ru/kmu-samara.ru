d6f624da51628c6c3d811e87b5e0e441db512224
Alexander Sabanov <alexsab@mail.ru>
2020-10-14 12:35:18 +0400

    upd gulpfile and packages

--- .gulpfile.js
+++ .gulpfile.js
@@ -1,85 +1,110 @@
-let projects = {
+// VARIABLES & PATHS
+let preprocessor = 'sass', // Preprocessor (sass, scss, less, styl)
+    fileswatch   = 'html,htm,txt,json,md,woff2,php', // List of files extensions for watching & hard reload (comma separated)
+    pageversion  = 'html,htm,php', // List of files extensions for watching change version files (comma separated)
+    imageswatch  = 'jpg,jpeg,png,webp,svg', // List of images extensions for watching & compression (comma separated)
+    online       = true, // If «false» - Browsersync will work offline without internet connection
+    basename     = require('path').basename(__dirname),
+    forProd      = [
+					'/**',
+					' * @author Alexsab.ru',
+					' */',
+					''].join('\n');
+
+const { src, dest, parallel, series, watch, task } = require('gulp'),
+	sass           = require('gulp-sass'),
+	cleancss       = require('gulp-clean-css'),
+	concat         = require('gulp-concat'),
+	browserSync    = require('browser-sync').create(),
+	uglify         = require('gulp-uglify-es').default,
+	autoprefixer   = require('gulp-autoprefixer'),
+	imagemin       = require('gulp-imagemin'),
+	newer          = require('gulp-newer'),
+	rsync          = require('gulp-rsync'),
+	del            = require('del'),
+	connect        = require('gulp-connect-php'),
+	header         = require('gulp-header'),
+	notify         = require('gulp-notify'),
+	rename         = require('gulp-rename'),
+	responsive     = require('gulp-responsive'),
+	pngquant       = require('imagemin-pngquant'),
+	merge          = require('merge-stream'),
+	// version        = require('gulp-version-number'),
+	// revAll         = require('gulp-rev-all'),
+	replace        = require('gulp-replace');
+
+if(typeof projects == 'undefined') 
+	global.projects = {};
+if(typeof port == 'undefined') 
+	global.port = 8100;
 
 
+projects.newstreetpunk_kmu = {
 
+	port: ++port,
 
+	base: basename,
+	dest: basename,
 
-	newstreetpunk_kmu: {
-
-		port: ++port,
-
-		base: base.newstreetpunk_kmu,
-		dest: base.newstreetpunk_kmu,
-
-		styles: {
-			src:	base.newstreetpunk_kmu + '/' + preprocessor + '/styles.'+preprocessor,
-			watch:    base.newstreetpunk_kmu + '/' + preprocessor + '/**/*.'+preprocessor,
-			dest:   base.newstreetpunk_kmu + '/css',
-			output: 'styles.css',
-		},
-
-		scripts: {
-			src: [
-				base.newstreetpunk_kmu + '/libs/jquery/dist/jquery.min.js',
-				base.newstreetpunk_kmu + '/libs/Magnific-Popup-master/jquery.magnific-popup.js',
-				base.newstreetpunk_kmu + '/libs/slick/slick.js',
-				// 'node_modules/slick-carousel/slick/slick.js',
-				base.newstreetpunk_kmu + '/libs/animate/animate-css.js',
-				base.newstreetpunk_kmu + '/libs/lazyload.min.js',
-				base.newstreetpunk_kmu + '/libs/waypoint.js',
-				base.newstreetpunk_kmu + '/js/map.js',
-				base.newstreetpunk_kmu + '/js/common.js', // Custom scripts. Always at the end
-			],
-			dest:       base.newstreetpunk_kmu + '/js',
-			output:     'scripts.min.js',
-		},
-
-		styles_new: {
-			src:	base.newstreetpunk_kmu + '/src/' + preprocessor + '/styles.'+preprocessor,
-			watch:    base.newstreetpunk_kmu + '/src/' + preprocessor + '/**/*.'+preprocessor,
-			dest:   base.newstreetpunk_kmu + '/css',
-			output: 'styles.css',
-		},
-
-		scripts_new: {
-			src: [
-				base.newstreetpunk_kmu + '/src/libs/jquery/dist/jquery.min.js',
-				base.newstreetpunk_kmu + '/src/libs/Magnific-Popup-master/jquery.magnific-popup.js',
-				base.newstreetpunk_kmu + '/src/libs/slick/slick.js',
-				// 'node_modules/slick-carousel/slick/slick.js',
-				base.newstreetpunk_kmu + '/src/libs/animate/animate-css.js',
-				base.newstreetpunk_kmu + '/src/libs/lazyload.min.js',
-				base.newstreetpunk_kmu + '/src/libs/waypoint.js',
-				base.newstreetpunk_kmu + '/src/js/map.js',
-				base.newstreetpunk_kmu + '/src/js/common.js', // Custom scripts. Always at the end
-			],
-			dest:       base.newstreetpunk_kmu + '/js',
-			output:     'scripts.min.js',
-		},
-
-		code: {
-			src: [
-				base.newstreetpunk_kmu  + '/**/*.{' + fileswatch + '}',
-			],
-		},
-
-		forProd: [
-			'/**',
-			' * @author https://github.com/newstreetpunk',
-			' * @editor https://github.com/alexsab',
-			' */',
-			''].join('\n'),
+	styles: {
+		src:	basename + '/' + preprocessor + '/styles.'+preprocessor,
+		watch:    basename + '/' + preprocessor + '/**/*.'+preprocessor,
+		dest:   basename + '/css',
+		output: 'styles.css',
 	},
 
+	scripts: {
+		src: [
+			basename + '/libs/jquery/dist/jquery.min.js',
+			basename + '/libs/Magnific-Popup-master/jquery.magnific-popup.js',
+			basename + '/libs/slick/slick.js',
+			// 'node_modules/slick-carousel/slick/slick.js',
+			basename + '/libs/animate/animate-css.js',
+			basename + '/libs/lazyload.min.js',
+			basename + '/libs/waypoint.js',
+			basename + '/js/map.js',
+			basename + '/js/common.js', // Custom scripts. Always at the end
+		],
+		dest:       basename + '/js',
+		output:     'scripts.min.js',
+	},
 
+	styles_new: {
+		src:	basename + '/src/' + preprocessor + '/styles.'+preprocessor,
+		watch:    basename + '/src/' + preprocessor + '/**/*.'+preprocessor,
+		dest:   basename + '/css',
+		output: 'styles.css',
+	},
 
+	scripts_new: {
+		src: [
+			basename + '/src/libs/jquery/dist/jquery.min.js',
+			basename + '/src/libs/Magnific-Popup-master/jquery.magnific-popup.js',
+			basename + '/src/libs/slick/slick.js',
+			// 'node_modules/slick-carousel/slick/slick.js',
+			basename + '/src/libs/animate/animate-css.js',
+			basename + '/src/libs/lazyload.min.js',
+			basename + '/src/libs/waypoint.js',
+			basename + '/src/js/map.js',
+			basename + '/src/js/common.js', // Custom scripts. Always at the end
+		],
+		dest:       basename + '/js',
+		output:     'scripts.min.js',
+	},
+
+	code: {
+		src: [
+			basename  + '/**/*.{' + fileswatch + '}',
+		],
+	},
+
+	forProd: [
+		'/**',
+		' * @author https://github.com/newstreetpunk',
+		' * @editor https://github.com/alexsab',
+		' */',
+		''].join('\n'),
 }
-
-
-
-
-
-
 
 
 
@@ -158,11 +183,10 @@ function newstreetpunk_kmu_watch_new() {
 	// watch(projects.newstreetpunk_kmu.code.src).on('change', browserSync.reload);
 };
 
-exports.newstreetpunk_kmu_browsersync = newstreetpunk_kmu_browsersync;
-exports.newstreetpunk_kmu_new = parallel(newstreetpunk_kmu_styles_new, newstreetpunk_kmu_scripts_new, newstreetpunk_kmu_browsersync, newstreetpunk_kmu_watch_new);
-exports.newstreetpunk_kmu = parallel(newstreetpunk_kmu_styles, newstreetpunk_kmu_scripts, newstreetpunk_kmu_browsersync, newstreetpunk_kmu_watch);
+// module.exports = newstreetpunk_kmu_browsersync;
+module.exports = parallel(newstreetpunk_kmu_styles_new, newstreetpunk_kmu_scripts_new, newstreetpunk_kmu_browsersync, newstreetpunk_kmu_watch_new);
+// module.exports = parallel(newstreetpunk_kmu_styles, newstreetpunk_kmu_scripts, newstreetpunk_kmu_browsersync, newstreetpunk_kmu_watch);
 
 
 /* newstreetpunk_kmu END */
-
 
