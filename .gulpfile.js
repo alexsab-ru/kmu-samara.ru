41779039f6506a975336ed5f6d3ec9f371763032
Alexander Sabanov <alexsab@mail.ru>
2020-11-30 07:51:15 +0400

    uncomment task

--- .gulpfile.js
+++ .gulpfile.js
@@ -233,7 +233,7 @@ function kmu_samara_run_jekyll(callback) {
 
 // exports.kmu_samara_jekyll = parallel(kmu_samara_styles_jekyll, kmu_samara_scripts_jekyll, kmu_samara_browsersync, kmu_samara_watch_jekyll);
 exports.kmu_samara_run_jekyll = parallel(kmu_samara_cd_jekyll, kmu_samara_styles_jekyll, kmu_samara_scripts_jekyll, kmu_samara_run_jekyll, kmu_samara_browsersync_jekyll, kmu_samara_watch_jekyll);
-// exports.kmu_samara = parallel(kmu_samara_styles, kmu_samara_scripts, kmu_samara_browsersync, kmu_samara_watch);
+exports.kmu = parallel(kmu_samara_styles, kmu_samara_scripts, kmu_samara_browsersync, kmu_samara_watch);
 exports.kmu_samara = parallel(kmu_samara_browsersync, kmu_samara_watch);
 
 
