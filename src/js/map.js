ca6a6d48b9421f6dafe234734f44d8cc42e4c636
Alexander Sabanov <alexsab@mail.ru>
2024-04-09 14:38:20 +0400

    change schedule

--- src/js/map.js
+++ src/js/map.js
@@ -11,7 +11,7 @@ $(function() {
                     <a href="mailto:bat.smr63@yandex.ru" class="dealer-email">📩 bat.smr63@yandex.ru</a><br><br> \
                     Заводское шоссе, 11<br>\
                     <a href="https://yandex.ru/maps/?z=15&ll=50.26109099999999,53.19987499999774&l=map&rtext=~53.199875,50.261091&origin=jsapi_2_1_76&from=api-maps"  target="_blank" class="dealer-phone">📍 Проложить маршрут</a><br>',
-                balloonContentFooter: 'ежедневно: 8:00 — 20:00',
+                balloonContentFooter: 'ежедневно: 8:00 — 17:00',
                 hintContent: 'Бумеранг-АвтоТранс'
             },
         ],
