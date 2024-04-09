02abb527023acfa3321ea5651724e16dc5bbd32a
Alexander Sabanov <alexsab@mail.ru>
2024-04-09 14:39:08 +0400

    format phone number

--- src/js/map.js
+++ src/js/map.js
@@ -7,7 +7,7 @@ $(function() {
                 position: [53.199875, 50.261091],
                 zoom: 15,
                 balloonContentHeader: 'Бумеранг-АвтоТранс',
-                balloonContentBody: '<a href="tel:88469722555" class="dealer-phone">☎️ 8 (846) 972-25-55</a><br>\
+                balloonContentBody: '<a href="tel:88469722555" class="dealer-phone">☎️ 8 (846) 97-22-555</a><br>\
                     <a href="mailto:bat.smr63@yandex.ru" class="dealer-email">📩 bat.smr63@yandex.ru</a><br><br> \
                     Заводское шоссе, 11<br>\
                     <a href="https://yandex.ru/maps/?z=15&ll=50.26109099999999,53.19987499999774&l=map&rtext=~53.199875,50.261091&origin=jsapi_2_1_76&from=api-maps"  target="_blank" class="dealer-phone">📍 Проложить маршрут</a><br>',
