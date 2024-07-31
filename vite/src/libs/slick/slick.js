abb7ee4e5a38776a1cfe9a95334a7b307fff4c06
Alexander Sabanov <alexsab@mail.ru>
2024-07-31 17:16:27 +0400

    fix jquery

--- vite/src/libs/slick/slick.js
+++ vite/src/libs/slick/slick.js
@@ -15,17 +15,7 @@
 
  */
 /* global window, document, define, jQuery, setInterval, clearInterval */
-(function(factory) {
-    'use strict';
-    if (typeof define === 'function' && define.amd) {
-        define(['jquery'], factory);
-    } else if (typeof exports !== 'undefined') {
-        module.exports = factory(require('jquery'));
-    } else {
-        factory(jQuery);
-    }
-
-}(function($) {
+(function($) {
     'use strict';
     var Slick = window.Slick || {};
 
@@ -2889,4 +2879,4 @@
         return _;
     };
 
-}));
+})(jQuery);
